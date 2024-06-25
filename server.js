9a33845727edb6725aa0c120167b08ed1f28a486
Fahad Tariq Aziz <fahadtariq7865999@gmail.com>
2024-06-25 21:59:49 +0500

    all-backend-code

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require ('express');
+const dbConnect = require('./database/index');
+const {PORT} = require('./config/index');
+const router = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+//const corsOptions = {
+//    Credentials : true,
+//    origin : ['http://localhost:3000']
+//}
+
+const app = express();
+
+app.use(cookieParser());
+
+//app.use(cors(corsOptions));
+
+app.use(
+    cors({
+        origin : function (origin , callback) {
+            return callback(null ,true);
+        },
+        optionsSuccessStatus : 200,
+        credentials: true,
+    })
+);
+
+app.use(express.json({limit : '50mb'}));
+
+app.use(cors());
+app.use(router);
+
+
+dbConnect();
+
+app.use('/storage', express.static('storage'));
+
+
+app.use(errorHandler);
+
+// just for testing now remove         app.get('/', (req,res) => res.json({msg : 'Hello WOrld'}));
+
+app.listen(PORT, console.log(`Backend is running on port : ${PORT}`));
